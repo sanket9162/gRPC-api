3c57adc611bac0079040528bdcbb6fb6aee2c148
sanket9162 <sanketgondhali55@gmail.com>
2025-11-02 10:48:58 +0530

    delete teacher from database

--- internal/api/handlers/teachers.go
+++ internal/api/handlers/teachers.go
@@ -2,11 +2,14 @@ package handlers
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/sanket9162/grpc-api/internal/models"
 	"github.com/sanket9162/grpc-api/internal/repositories/mongodb"
 	pb "github.com/sanket9162/grpc-api/proto/gen"
 	"github.com/sanket9162/grpc-api/utils"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -49,4 +52,46 @@ func (s *Server) UpdateTeachers(ctx context.Context, req *pb.Teachers) (*pb.Teac
 		return nil, status.Error(codes.Internal, err.Error())
 	}
 	return &pb.Teachers{Teachers: updatedTeachers}, nil
+}
+
+func (s *Server) DeleteTeachers(ctx context.Context, req *pb.TeacherIds) (*pb.DeleteTeachersConfirmation, error) {
+	ids := req.GetIds()
+	var teacherIdsToDelete []string
+	for _, v := range ids {
+		teacherIdsToDelete = append(teacherIdsToDelete, v.Id)
+	}
+	client, err := mongodb.CreateMongoClient()
+	if err != nil {
+		return nil, utils.ErrorHandler(err, "internal error")
+	}
+	defer client.Disconnect(ctx)
+
+	objectIds := make([]primitive.ObjectID, len(teacherIdsToDelete))
+	for i, id := range teacherIdsToDelete {
+		objectId, err := primitive.ObjectIDFromHex(id)
+		if err != nil {
+			return nil, utils.ErrorHandler(err, fmt.Sprintf("incorrect id%v", id))
+		}
+		objectIds[i] = objectId
+	}
+	filter := bson.M{"_id": bson.M{"$in": objectIds}}
+	result, err := client.Database("school").Collection("teacher").DeleteMany(ctx, filter)
+	if err != nil {
+		return nil, utils.ErrorHandler(err, "internal error")
+	}
+
+	if result.DeletedCount == 0 {
+		return nil, utils.ErrorHandler(err, "no teachers were deleted")
+	}
+
+	deletedIds := make([]string, result.DeletedCount)
+	for i, id := range objectIds {
+		deletedIds[i] = id.Hex()
+	}
+
+	return &pb.DeleteTeachersConfirmation{
+		Status:     "Teachers successfully deleted",
+		DeletedIds: deletedIds,
+	}, nil
+
 }
